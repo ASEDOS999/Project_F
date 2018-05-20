90e5a4408a840a6a09387f6a8519e83d085143ab
ased <kuruzov2014@mail.ru>
2018-05-20 23:53:21 +0300

    Add new class for image's segmentation

---
+++ segmentation.py
@@ -0,0 +1,59 @@
+class segmentation():
+	strings = []
+	letters = []
+	def string_border(self, im):
+	        begin_string, end_string = 0, 0
+
+	        #False = there aren't black pixels; True
+		previous, str = False, False
+		upd = 0
+
+		for y in range(im2.size[1]):
+			previous = str
+			str = False
+			for x in range(im2.size[0]):
+				pix = im2.getpixel((x,y))
+				if pix != 255:
+					#In this pixel's string there are black pixels
+					str = True
+
+				if str == True and previous == False:
+					begin_string = y
+					break
+
+
+				if str == False and x == im2.size[0] - 1:
+					#In this pixel's string there aren't black pixels
+					if previous == True:
+						end_string = y - 1
+						self.strings.append((begin_string, end_string))
+		return 0
+
+	def letter_border(self, im):
+		inletter = False
+		foundletter = False
+		start_x = 0
+		start_y = 0
+		end_x = 0
+		end_y = 0
+
+		for string in strings:
+			start = string[0]
+			end = string[1]
+			for x in range(im2.size[0]):
+				for y in range(start, end, 1):
+					pix = im2.getpixel((x,y))
+					if pix == 0:
+						inletter = True
+
+				if foundletter == False and inletter == True:
+						foundletter = True
+						start_x = x
+
+				if foundletter == True and inletter == False:
+					foundletter = False
+					end_x = x
+					letters.append((start_x, start, end_x, end))
+
+				inletter=False
+		return 0
