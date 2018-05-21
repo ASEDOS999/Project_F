7722533c19f695921b44ea9fb58851362696da04
ased <kuruzov2014@mail.ru>
2018-05-21 17:44:12 +0300

    Connectivity check

--- segmentation.py
+++ segmentation.py
@@ -1,3 +1,4 @@
+#This module has class for segmentation into strings and letters
 class segmentation():
 	strings = []
 	letters = []
@@ -36,21 +37,39 @@ class segmentation():
 		start_y = 0
 		end_x = 0
 		end_y = 0
+		last_column, current_column = [], []
 
 		for string in self.strings:
 			start = string[0]
 			end = string[1]
 			for x in range(im.size[0]):
+				last_column = current_column
+				current_column = []
+				sum = 0
+				k = 1
+
 				for y in range(start, end, 1):
 					pix = im.getpixel((x,y))
+					current_column.append(pix)
 					if pix == 0:
 						inletter = True
-
+				"""
+				#Connectivity_check
+				if x == 0:
+					k = 1
+				else:
+					for i in range(len(current_column)):
+						if last_column[i] != 255 and current_column[i] != 255:
+							sum += 1
+					if sum > 0:
+						k = 1
+				"""
 				if foundletter == False and inletter == True:
-						foundletter = True
-						start_x = x
+					foundletter = True
+					start_x = x
 
-				if foundletter == True and inletter == False:
+				#Finded letter has not more pixel in this column or this letter is not connected with current pixel
+				if foundletter == True and (inletter == False or k == 0):
 					foundletter = False
 					end_x = x
 					self.letters.append((start_x, start, end_x, end))
