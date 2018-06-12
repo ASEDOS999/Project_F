7a2039ab50b17c9874e9f779f97ab2949346eea8
ased <kuruzov2014@mail.ru>
2018-06-12 08:33:47 +0300

    Test with a small letter's set

---
+++ test_main.py
@@ -0,0 +1,9 @@
+import main
+
+set = []
+set.append(('A', './SET/A'))
+set.append(('B', './SET/B'))
+set.append(('C', './SET/C'))
+set.append(('D', './SET/D'))
+
+main.text(set, 'Desktop/PROJECTS/Project_F/ABCD/T_1.gif')
