d7caae4b1bd644753d046a4d0b284ec1a2078290
ased <kuruzov2014@mail.ru>
2018-06-20 21:00:39 +0300

    Debug label's size and add '\n'

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append("./Segmentation")
+sys.path.append("./Recognition")
+sys.path.append("./Interface")
+
 import math
 import PIL
 import vector
@@ -5,6 +10,7 @@ import recog
 import os
 from segmentation import segmentation
 from PIL import Image
+
 
 def text(set, path):
 	im = Image.open("../../../%s"%(path))
@@ -53,7 +59,7 @@ def text(set, path):
 
 	delta = 0
 	for letter in letters:
-		delta = letter[2] - letter[0]
+		delta = delta + (letter[2] - letter[0])
 	delta = delta / len(letters)
 
 
@@ -66,9 +72,18 @@ def text(set, path):
 		if letter[1] == start and letter[3] == end:
 			delta = letter[0] - prev
 			if delta >= label:
-				set_label.append(place_label)
+				set_label.append((place_label, ' '))
+			else:
+				set_label.append((place_label, ''))
+		else:
+			if start != 0 or end != 0:
+				set_label.append((place_label, '\n'))
+			else:
+				set_label.append((place_label, ''))
 		place_label += 1
 		start, end, prev = letter[1], letter[3], letter[2]
+
+	#SIMPLE RECOGNITION OF SYMBOLS
 
 	imageset = []
 	for i in set:
@@ -76,28 +91,28 @@ def text(set, path):
 			temp = Image.open('%s/%s'%(i[1], img))
 			imageset.append((i[0], temp))
 
-	#SIMPLE RECOGNITION OF SYMBOLS
-
 	v = vector.Common_Vector_Compare()
 	text_letter = []
+	z = 100
 	for letter in letters:
+		v.count = z
 		guess = []
 		cutlet = im2.crop(letter)
 		guess = recog.symb_recog(v, cutlet, imageset)
 		guess.sort()
 		text_letter.append(guess[len(guess) - 1][1])
-
+		z = z + 100
 	output = open('out.txt', 'w')
 
 	number_letter = 0
 	k = set_label.pop(0)
 	for i in text_letter:
-		output.write(i)
-		number_letter += 1
-		if number_letter == k:
-			output.write(' ')
+		if number_letter == k[0]:
+			output.write(k[1])
 			if len(set_label) > 0:
 				k = set_label.pop(0)
+		output.write(i)
+		number_letter += 1
 	text_letter = []
 	output.close()
 
